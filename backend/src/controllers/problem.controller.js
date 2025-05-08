ea762c2f8002dd393afcffad8696163b1703aa9c
amitesh08 <aamitesh125@gmail.com>
2025-05-08 21:57:38 +0530

    added problem routes

---
+++ backend/src/controllers/problem.controller.js
@@ -0,0 +1,13 @@
+import { db } from "../libs/db.js";
+
+export const createProblem = async (req, res) => {};
+
+export const getAllProblems = async (req, res) => {};
+
+export const getAllProblemById = async (req, res) => {};
+
+export const updateProblem = async (req, res) => {};
+
+export const deleteProblem = async (req, res) => {};
+
+export const getAllProblemsSolvedByUser = async (req, res) => {};
